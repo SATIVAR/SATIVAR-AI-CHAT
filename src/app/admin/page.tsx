484644c72d4e6cf5c51d1beee3c9ed62dd2b27f1
SATIVAR Dispensário Educacional <sativaroficial@gmail.com>
2025-08-28 13:25:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,8 @@ import { doesOwnerExist } from '@/lib/firebase/auth-admin';
 import RegisterOwnerForm from '@/components/admin/register-owner-form';
 import LoginOwnerForm from '@/components/admin/login-owner-form';
 
+export const revalidate = 0;
+
 export default async function AdminAccessPage() {
   const ownerExists = await doesOwnerExist();
 
